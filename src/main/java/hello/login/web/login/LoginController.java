7fcd70088f3542e2b71ebbc900179c59b1d3e4eb
ats3059 <ats3059@naver.com>
2021-10-16 23:17:22 +0900

    filter login process

--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -14,6 +14,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.*;
 import java.net.http.HttpRequest;
@@ -70,7 +71,7 @@ public class LoginController {
 
     }
 
-    @PostMapping("/login")
+//    @PostMapping("/loginin")
     public String loginV3(@Validated @ModelAttribute LoginForm loginForm, BindingResult bindingResult, HttpServletRequest request){
         if(bindingResult.hasErrors()){
             return "login/loginForm";
@@ -88,6 +89,30 @@ public class LoginController {
         //세션에 로그인 회원 정보저장
         session.setAttribute(SessionConst.LOGIN_MEMBER,login);
         return "redirect:/";
+
+    }
+
+
+    @PostMapping("/login")
+    public String loginV4(@Validated @ModelAttribute LoginForm loginForm, BindingResult bindingResult,
+                          @RequestParam(defaultValue = "/") String redirectParam,
+                          HttpServletRequest request){
+        if(bindingResult.hasErrors()){
+            return "login/loginForm";
+        }
+        Member login = loginService.login(loginForm.getLoginId(), loginForm.getPassword());
+
+        if(login == null){
+            bindingResult.reject("loginFail","아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+        //로그인 성공처리
+        //세션이 있으면 있는 세션 반환 , 없으면 신규 세션을 ㅅ생성
+        HttpSession session = request.getSession();
+        //세션에 로그인 회원 정보저장
+        session.setAttribute(SessionConst.LOGIN_MEMBER,login);
+        return "redirect:"+redirectParam;
 
     }
 
